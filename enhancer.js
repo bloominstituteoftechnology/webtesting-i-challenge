2d920f840e5b3c41e609c3163c10dd587a458737
Emmanium <pradoe.ep@gmail.com>
2019-01-30 16:42:55 -0700

    added success method to enhancer.js

--- enhancer.js
+++ enhancer.js
@@ -1,16 +1,43 @@
 module.exports = {
   
   success: (item) => {
-
-  },
-
-  fail: (item) => {
     if (item.enhancement <= 14 && item.durability < 25) {
       throw new Error('Durability is below 25; item cannot be enhanced');
     }
 
     if (item.enhancement >= 15 && item.durability < 10) {
       throw new Error('Durability is below 10; item cannot be enhanced');
+    }
+
+    if (item.enhancement >= 0 && item.enhancement < 15) {
+      item.enhancement += 1;
+      item.name = `[+${item.enhancement} ${item.name}]`
+    } else if (item.enhancement === 15) {
+      item.enhancement += 1;
+      item.name = `[PRI] ${item.name}`
+    } else if (item.enhancement === 16) {
+      item.enhancement += 1;
+      item.name = `[DUO] ${item.name}`
+    } else if (item.enhancement === 17) {
+      item.enhancement += 1;
+      item.name = `[TRI] ${item.name}`
+    } else if (item.enhancement === 18) {
+      item.enhancement += 1;
+      item.name = `[TET] ${item.name}`
+    } else if (item.enhancement === 19) {
+      item.enhancement += 1;
+      item.name = `[PEN] ${item.name}`
+    }
+    return item;
+  },
+
+  fail: (item) => {
+    if (item.type === 'armor' && item.enhancement <= 5) {
+      throw new Error('Armor enhancement is level 5 or under, cannot fail');
+    }
+
+    if (item.type === 'weapon' && item.enhancement <= 7) {
+      throw new Error('Weapon enhancement is level 7 or under, cannot fail');
     }
 
     if (item.enhancement >= 0 && item.enhancement <= 14) {
