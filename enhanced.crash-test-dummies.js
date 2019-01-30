da67e4a3ac5ef94cd361ea19c1b7a17728fa2e3e
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-29 23:15:47 -0700

    The test data has been added, so I can make sure the tests are working.

---
+++ enhanced.crash-test-dummies.js
@@ -0,0 +1,31 @@
+let armory = [
+    {
+        name: 'torch',
+        type: 'weapon',
+        durability: 80,
+        enhancement: 10
+    }
+    ,
+    {
+        name: 'gold-plate',
+        type: 'armour',
+        durability: 100,
+        enhancement: 15
+    }
+    ,
+    {
+        name: 'axe',
+        type: 'weapon',
+        durability: 65,
+        enhancement: 4
+    }
+    ,
+    {
+        name: 'invisible-plate',
+        type: 'armour',
+        durability: 100,
+        enhancement: 12
+    }
+]
+
+
