56de465a28fb7b5f0aa4dea315f517d0e9332558
marcusbass323 <marcus@bassconsultantgroup.com>
2019-01-30 11:07:12 -0800

    enhancements.js created with success, fail and repair methods added

---
+++ enhancements.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    //a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
+    success: () => {
+    
+    },
+
+    //a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
+    fail: () => {
+
+    },
+    //a repair(item) method that accepts an item object and returns a new item with the durability restored to 100.
+    repair: () => {
+
+    }
+
+}
