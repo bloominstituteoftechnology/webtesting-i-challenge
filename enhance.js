4cdcef68356d5277660c49380f68b09ae8ddfabd
Jason Hedrick <jasonleehedrick@gmail.com>
2019-01-30 19:11:21 -0800

    Built the tests for enhance.success and the functionality.

---
+++ enhance.js
@@ -0,0 +1,28 @@
+module.exports = {
+    success:(item) => {
+        item.enhancement+=1;
+        let prefix = {
+            16: 'PRI',
+            17: 'DUO',
+            18: 'TRI',
+            19: 'TET',
+            20: 'PEN' 
+        }
+        if (item.enhancement < 16) {
+            item.prefix = item.enhancement;
+        }
+        else {
+            item.prefix = prefix[item.enhancement];
+        }
+        item.name = `[${item.prefix}] ${item.baseName}`
+        return item;
+    },
+
+    fail:(item) => {
+
+    },
+
+    repair:(item) => {
+
+    }
+}
