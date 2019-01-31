6ecf6a6d221fc979f3fb8c98f2bb4def4827be21
mglittleton <michaelglittleton@gmail.com>
2019-01-31 02:12:54 -0500

    fail and success functions have been finished. success has been tested

--- items.js
+++ items.js
@@ -1,30 +1,87 @@
 const cl = console.log;
 
+const enhTree = {
+  0: "",
+  1: "[+1] ",
+  2: "[+2] ",
+  3: "[+3] ",
+  4: "[+4] ",
+  5: "[+5] ",
+  6: "[+6] ",
+  7: "[+7] ",
+  8: "[+8] ",
+  9: "[+9] ",
+  10: "[+10] ",
+  11: "[+11] ",
+  12: "[+12] ",
+  13: "[+13] ",
+  14: "[+14] ",
+  15: "[+15] ",
+  16: "[PRI] ",
+  17: "[DUO] ",
+  18: "[TRI] ",
+  19: "[TET] ",
+  20: "[PEN] "
+};
+
 const success = item => {
-  return item;
+  const modItem = Object.assign({}, item);
+  if (modItem.enhancement < 20) {
+    modItem.enhancement += 1;
+  }
+  setName(modItem);
+  return modItem;
 };
 
 const fail = item => {
-  return item;
+  const modItem = Object.assign({}, item);
+  const lowLimit = modItem.type === "armor" ? 5 : 7;
+  const enhancement = modItem.enhancement;
+  if (enhancement <= lowLimit) {
+    setName(modItem);
+    return modItem;
+  }
+  if (enhancement < 15) {
+    if (modItem.durability < 25) {
+      setName(modItem);
+      return modItem;
+    }
+    modItem.durability -= 5;
+    setName(modItem);
+    return modItem;
+  } 
+  modItem.durability -= 10;
+  modItem.enhancement =
+    enhancement > 16 ? modItem.enhancement - 1 : modItem.enhancement;
+  setName(modItem);
+  cl(modItem)
+  return modItem;
 };
 
 const repair = item => {
-  return item;
+  const modItem = Object.assign({}, item);
+  return modItem;
 };
 
-const sword = {
-  name: "Excalibur",
-  type: "weapon",
-  durability: 60, // max of 100
-  enhancement: "14" // avail [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,PRI,DUO,TRI,TET,PEN]
+const setName = item => {
+  item.name = `${enhTree[item.enhancement]}${item.blandName}`;
 };
 
-const helmet = {
-  name: "Perseus' Helm",
-  type: "armor",
-  durability: 90,
-  enhancement: "PRI"
-};
+// const sword = {
+//   name: "",
+//   blandName: "Excalibur",
+//   type: "weapon",
+//   durability: 60, // max of 100
+//   enhancement: 14 // avail [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,PRI,DUO,TRI,TET,PEN]
+// };
+
+// const helmet = {
+//   name: "",
+//   blandName: "Perseus' Helm",
+//   type: "armor",
+//   durability: 90,
+//   enhancement: 16
+// };
 
 module.exports = {
   success,
--- items.test.js
+++ items.test.js
@@ -2,17 +2,19 @@ const items = require("./items");
 const cl = console.log;
 
 const sword = {
-  name: "[+14] Excalibur",
+  name: "",
+  blandName: "Excalibur",
   type: "weapon",
   durability: 60, // max of 100
-  enhancement: "+14" // avail [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,PRI,DUO,TRI,TET,PEN]
+  enhancement: 20 // avail [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,PRI,DUO,TRI,TET,PEN]
 };
 
 const helmet = {
-  name: `[PRI] Perseus Helm`,
+  name: "",
+  blandName: "Perseus' Helm",
   type: "armor",
   durability: 90,
-  enhancement: "PRI"
+  enhancement: -1
 };
 
 const possEnh = [
@@ -40,25 +42,25 @@ const possEnh = [
 ];
 
 describe("success method", () => {
-  // the enhancement is included in the list of possible enhancement levels
-  test("enhancement of items", () => {
-    expect(possEnh.includes(items.success(sword).enhancement)).toBe(true);
-  });
-
-  // the name is displayed correctly
+  // the name is displayed correctly with correct level
+  test('enhanced name of items', () => {
+    expect(items.success(sword).name).toBe('[PEN] Excalibur')
+  })
   // the name is displayed correctly when level is +0
-  // the enchancement level goes up by exactly 1
+  test('enhanced name of items at level 0', () => {
+    expect(items.success(helmet).name).toBe("Perseus' Helm")
+  })
 });
 
 describe("failure method", () => {
-  // the enhancement is included in the list of possible enhancement levels
-  // the name is displayed correctly
-  // the name is displayed correctly when level is +0
-  // the enhancement level goes down by exactly 1
-  // armor of level 5 or below cannot be changed
+
+  // the name is displayed correctly with proper level
+  // the name is displayed correctly when level is +0 and no fail allowed
+  // the enhancement level goes down by exactly 1 only if level is over 16
   // weapon of level 7 or below cannot be changed
   // the durability is decreased by 5 if level is under 15
   // the durability is decreased by 10 if level is 15 or greater
+  // durability decreased by 10, but level stays the same at level 15 or 16
   // QUESTION on "cannot be enhanced section of instruction"
   // the durability cannot be less than 20 if the level drops below 15
   // the durability cannot be less than 0 if the level drops below PEN
