288134b7c0864aeb7106be38f0296de3bfeb2041
jbrock1981 <jbrock1981@gmail.com>
2019-01-30 22:01:50 -0600

    built success test

--- index.test.js
+++ index.test.js
@@ -1,1 +1,28 @@
 const {success, fail, repair} = require('./index');
+
+describe('test outcomes', () => {
+    describe('success outcome', () => {
+        const item = {
+            beforeName: 'test weapon',
+            name: 'test weapon',
+            type: 'weapon',
+            durability: 100,
+            enhancement: 'PEN'
+        };
+        const expected = {
+            beforeName: 'test weapon',
+            name: '[PEN] test weapon',
+            type: 'weapon',
+            durability: 100,
+            enhancement: 'PEN'
+        };
+        it('should give back same item', () => {
+            const actual = success(item);
+            expect(actual).toEqual(expected)
+        });
+        it('type is weapon or armor', () => {
+            expect(item.type).toBe('weapon' || 'armor')
+        });
+    });
+    
+});
