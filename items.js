977010c2b4a36dc24749528c82356394d0c568f6
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-31 13:43:37 -0600

    Se4t up file to contain armor and weapons items

---
+++ items.js
@@ -0,0 +1,43 @@
+//file containing items used in enhancement functions
+
+
+    const sword = {
+        name: 'sword',
+        type: 'weapon',
+        durability: 100,
+        enhancement: 1
+    }
+    const upgraded_sword = {
+        name: 'upgraded_sword',
+        type: 'weapon',
+        durability: 100,
+        enhancement: 2
+    }
+    const mace = {
+        name: 'mace',
+        type: 'weapon',
+        durability: 70,
+        enhancement: 14
+    }
+    
+    const nunchucks = {
+        name: 'nunchucks',
+        type: 'weapon',
+        durability: 50,
+        enhancement: 'PEN'
+    }
+
+
+    const standard_shield = {
+        name: 'standard_shield',
+        type: 'armor',
+        durability: 45,
+        enhancement: 12
+    }
+
+    const gloves = {
+        name: 'gloves',
+        type: 'armor',
+        durability: 20,
+        enhancement: 10
+    }
