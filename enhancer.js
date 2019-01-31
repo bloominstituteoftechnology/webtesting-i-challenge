f14e98d2752d7e95b6a7d76d00b6daaf1d04d0e6
BrendonKootz <brendonkootz@gmail.com>
2019-01-30 20:43:00 -0600

    Base item class hammered out

--- enhancer.js
+++ enhancer.js
@@ -1,3 +1,29 @@
 module.exports = {
 
+class Item {
+	constructor(name,type,durability,enhancement){
+		this.name = name;
+		this.type = type;
+		this.durability = durability = 100;
+		this.enhancement = enhancement = 0;
+	}
+	
+	function enhance(this.enhancement){
+		if(this.enhancement !=== 100){
+			return this.ehancement++;
+		} else {
+			return console.log("MAX Enhancement reached!!!");
+		}
+	}
+	
+	function degrade(this.durability){
+		if(this.durability === 0){return "Item broken lol";} else {
+			return (
+				console.log(durability);
+				this.durability--;
+				console.log(durability);
+			);
+		}
+	}
+
 };
