00d3eece97eee2bf3aba3ecf135b8990c7d3fd78
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-31 13:43:51 -0600

    Set up test suite

---
+++ enhancement.test.js
@@ -0,0 +1,10 @@
+//file for writing tests for enhancement functions
+
+const enhancements = require('./enhancements');
+const 
+
+describe('enhancement success function ', ()=>{
+    test('increase in enhancement by 1 and name updated', () =>{
+        expect()
+    })
+})
