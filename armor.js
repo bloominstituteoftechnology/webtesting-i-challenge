f62d3dba387a7848cebba28bafbf81da42cfb104
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-01-30 21:09:38 -0600

    created an armor file

---
+++ armor.js
@@ -0,0 +1,38 @@
+const shield = {
+    name: 'Shield',
+    type: 'armor',
+    durability: 100,
+    enhancement: 7
+}
+
+const forceField = {
+    name: 'Force Field',
+    type: 'armor',
+    durability: 100,
+    enhancement: 'TRI'
+}
+
+const invisibleShield = {
+    name: 'Invisible Shield',
+    type: 'armor',
+    durability: 100,
+    enhancement: 12
+}
+
+const kneePads = {
+    name: 'Knee Pads',
+    type: 'armor',
+    durability: 100,
+    enhancement: 2
+}
+
+const chestGuard = {
+    name: 'Chest Guard',
+    type: 'armor',
+    durability: 100,
+    enhancement: 5
+}
+
+module.exports = {
+    shield, forceField, invisibleShield, kneePads, chestGuard
+}
