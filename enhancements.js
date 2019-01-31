a718a2221a9fb22888a7f6c626a3f0c4da8872c4
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-31 13:42:57 -0600

    Set up file to contain functions

---
+++ enhancements.js
@@ -0,0 +1,16 @@
+//file containing enhancement/modification functions
+
+module.exports = {
+    
+    success : (item) =>{
+
+    },
+    
+    fail : (item) =>{
+
+    },  
+
+    repair : (item) =>{
+
+    }
+}
