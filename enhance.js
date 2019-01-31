793be7647f4d99f6dcb8475724000aef50e74acd
shannonhealy <sahealy525@gmail.com>
2019-01-31 10:47:05 -0500

    implementation of fail function

--- enhance.js
+++ enhance.js
@@ -41,7 +41,57 @@ const success = item => {
 };
 
 const fail = item => {
+    if (typeof item.name !== "string"
+        || typeof item.durability !== "number"
+        || typeof item.enhancement !== "number"
+        || item.enhancement > 20
+        || item.enhancement < 0
+        || item.durability > 100
+        || item.durability < 0
+        || (item.type !== "weapon" && item.type !== "armor")) {
+        return {error: "Malformed item data"}
+    } else if (item.enhancement === 20) {
+        return {error: "Cannot enhance an item above level 20 (PEN)"}
+    }
 
+    let newItem = item;
+
+    if (item.enhancement <= 14) {
+        newItem.durability -= 5;
+        if (newItem.durability < 20)
+            newItem.durability = 20;
+    } else {
+        newItem.durability -= 10;
+        if (newItem.durability < 0)
+            newItem.durability = 0;
+        if (newItem.enhancement > 16) {
+            newItem.enhancement--;
+
+            //get name without enhancement number prefix
+            let baseName = "";
+            if (item.name.charAt(0) === "[") {
+                nameArr = item.name.split(" ");
+                for (let i = 1; i < nameArr.length; i++) {
+                    baseName += nameArr[i];
+                    if (i !== nameArr.length - 1) {
+                        baseName += " ";
+                    }
+                }
+            } else {
+                baseName = item.name;
+            }
+
+            //set new item name
+            if (newItem.enhancement <= 15) {
+                newItem.name = `[+${newItem.enhancement}] ` + baseName;
+            } else {
+                let titleArr = ["PRI", "DUO", "TRI", "TET", "PEN"];
+                newItem.name = `[${titleArr[newItem.enhancement-16]}] ` + baseName;
+            }
+        }
+    }
+
+    return newItem;
 };
 
 const repair = item => {
