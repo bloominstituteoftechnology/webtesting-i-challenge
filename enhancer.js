bacb19ed753b41369275e410c6eecca0b65db934
paulapivat <paul.apivat@gmail.com>
2019-01-31 08:48:05 +0700

    add enhancer.test.js

--- enhancer.js
+++ enhancer.js
@@ -1,14 +1,17 @@
 module.exports = {
 
     success: (item) => {
-
+        // a method that accepts an item object
+        // returns a new item object modified according to rules for enhancement success
     },
 
     fail: (item) => {
-
+        // a method that accepts an item object
+        // returns a new item object modified according to rules for enhancement failure
     },
 
     repair: (item) => {
-
+        // a method that accepts an item object
+        // returns a new item with durability restored to 100
     },
 }
---
+++ enhancer.test.js
@@ -0,0 +1,27 @@
+const enhancer = require('./enhancer');
+
+const item = {
+    name: "axe",
+    type: "weapon",
+    durability: 100,
+    enhancement: 10,    // start 0, max 'PEN'
+}
+
+//describe enhancement success
+test('should do someting', () => {
+    // Arrange: setup the world
+
+    // Act: execute the tested code
+
+    // Assert: check the results are correct
+})
+
+
+//describe enhancement failure
+test('should do someting', () => {
+    // Arrange: setup the world
+
+    // Act: execute the tested code
+
+    // Assert: check the results are correct
+})
