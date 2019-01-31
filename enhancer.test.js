43cb4246f9e708da6614e28e2822a53507e11c99
unknown <kyleteeter12@gmail.com>
2019-01-30 21:39:16 -0600

    Adding first test case

---
+++ enhancer.test.js
@@ -0,0 +1,14 @@
+const enhancer = require('.enhancer');
+
+const repairedWeapon = {
+    name: 'Hammer',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 12
+}
+
+describe('weapon repair', () => {
+    test('restore durability to 100', () => {
+        expect()
+    })
+})
