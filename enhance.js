4b7f510565a89c26b373989a1527857bc9f63229
Robin U Familara <robin.familara@cgius.com>
2019-01-31 21:37:02 -0600

    Build enhance.js fail test and functionality

--- enhance.js
+++ enhance.js
@@ -1,6 +1,10 @@
 module.exports = {
     success:(item) => {
+        if (item.enhancement <= 14 && item.durability <= 25) return item;
+        if (item.enhancement > 14 && item.durability <= 10) return item;
+
         item.enhancement+=1;
+        if (item.enhancement < 20) item.enhancement = 20;
         let prefix = {
             16: 'PRI',
             17: 'DUO',
@@ -18,11 +22,39 @@ module.exports = {
         return item;
     },
 
-     fail:(item) => {
+    fail:(item) => {
+        if (item.enhancement <= 14 && item.durability > 25) {
+            item.durability -= 5;
+        }
+        else if (item.enhancement > 14 && item.durability > 10) {
+            item.durability -= 10;
+        }
 
-     },
+        if (item.enhancement < 16) {
+            item.enhancement -= 1;
+        }
 
-     repair:(item) => {
+        let prefix = {
+            16: 'PRI',
+            17: 'DUO',
+            18: 'TRI',
+            19: 'TET',
+            20: 'PEN' 
+        }
 
-     }
+        if (item.enhancement < 16) {
+            item.prefix = item.enhancement;
+        }
+        else {
+            item.prefix = prefix[item.enhancement];
+        }
+        item.name = `[${item.prefix}] ${item.baseName}`
+
+        return item;
+    },
+
+    repair:(item) => {
+        item.durability = 100;
+        return item;
+    }
 }
--- enhance.test.js
+++ enhance.test.js
@@ -1,6 +1,6 @@
 const enhance = require('./enhance');
 
- const dagger = {
+const dagger = {
     baseName: 'Stilleto',
     name: 'Stilleto',
     type: 'weapon',
@@ -9,7 +9,16 @@ const enhance = require('./enhance');
     prefix: '',
 }
 
- describe('Testing for enhancement success', () => {
+const sword = {
+    baseName: 'Falchion',
+    name: 'Falchion',
+    type: 'weapon',
+    durability: '100',
+    enhancement: 17,
+    prefix: '',
+}
+
+describe('Testing for enhancement success', () => {
     test('Testing that enhance.success returns a dagger object.', () => {
         expect(enhance.success(dagger)).toEqual(dagger);
     });
@@ -17,10 +26,31 @@ const enhance = require('./enhance');
         expect(dagger.enhancement).toBeGreaterThan(0);
     });
     test('Testing that enhance.success alters the name with the enhancement.', () => {
-        expect(dagger.name).toEqual(`[${dagger.prefix}] Stilleto`);
+        expect(dagger.name).toEqual(`[${dagger.prefix}] ${dagger.baseName}`);
     })
-    while(dagger.enhancement < 20) {
-        enhance.success(dagger);
-        console.log(dagger);
-    }
-})
+    
+});
+
+describe('Testing for enhancement failure', () => {
+    test('Testing that enhance.fail returns a sword object.', () => {
+        expect(enhance.fail(sword)).toEqual(sword);
+    });
+    test('Testing that enhance.fail decrements the durability.', () => {
+        expect(sword.durability).toBeLessThan(100);
+    });
+    test('Testing that enhance.fail removes an enhancement level if its enhancement is greater than 16.', () => {
+        expect(sword.enhancement).toBeLessThan(20);
+    });
+    test('Testing that enhance.fail alters the name with the enhancement.', () => {
+        expect(sword.name).toEqual(`[${sword.prefix}] ${sword.baseName}`);
+    })
+});
+
+describe('Testing enhance.repair', () => {
+    test('Testing that enhance.repair returns an item object', () => {
+        expect(enhance.repair(sword)).toEqual(sword);
+    });
+    test('Testing that enhance.repair sets the durabilty to 100.', () => {
+        expect(sword.durability).toEqual(100);
+    });
+});
