77c44a319739fcec6f215f425a29661ab7b47d5e
jbrock1981 <jbrock1981@gmail.com>
2019-02-03 05:04:58 -0600

    mvp

--- index.js
+++ index.js
@@ -1,42 +1,69 @@
-
-
-success = (item) => {
-    testItem = {
-        beforeName: item.beforeName,
-        name: `[+${item.enhancement+1}] ${item.beforeName}`,
-        type: item.type,
-        durability: item.durability,
-        enhancement: item.enhancement+1
-    }
-    if (item.enhancement === 15) {
-        testItem.name = `[PRI] ${item.beforeName}`,
-        testItem.enhancement = `PRI`
-    } else if (item.enhancement === 'PRI') {
-        testItem.name = `[DUO] ${item.beforeName}`,
-        testItem.enhancement = 'DUO'
-    } else if (item.enhancement === 'DUO') {
-        testItem.name = `[TRI] ${item.beforeName}`,
-        testItem.enhancement = 'TRI'
-    } else if (item.enhancement === 'TRI') {
-        testItem.name = `[TET] ${item.beforeName}`,
-        testItem.enhancement = 'TET'
-    } else if (item.enhancement === 'TET' || item.enhancement === 'PEN') {
-        testItem.name = `[PEN] ${item.beforeName}`,
-        testItem.enhancement = 'PEN'
-    }
-    return testItem;
-}
-
-fail = (item) => {
-
-}
-
-repair = (item) => {
-
-}
-
 module.exports = {
-    success,
-    fail,
-    repair
+    success: (item) => {
+        const itemName = item.unenhancedName;
+        if (item.enhancement <= 14 && item.durability < 25) {
+            throw new Error('Durability is below 25; item cannot be enhanced');
+        }
+        if (item.enhancement >= 15 && item.durability < 10) {
+            throw new Error('Durability is below 10, item cannot be enhanced');
+        }
+        if (item.enhancement === 20) {
+            throw new Error('Item enhancement is at max');
+        }
+        if (item.enhancement >= 0 && item.enhancement < 15) {
+            item.enhancement += 1;
+            item.name = `[+${item.enhancement}] ${itemName}`
+        } else if (item.enhancement === 15) {
+            item.enhancement += 1;
+            item.name = `[PRI] ${itemName}`
+        } else if (item.enhancement === 16) {
+            item.enhancement += 1;
+            item.name = `[DUO] ${itemName}`
+        } else if (item.enhancement === 17) {
+            item.enhancement += 1;
+            item.name = `[TRI] ${itemName}`
+        } else if (item.enhancement === 18) {
+            item.enhancement += 1;
+            item.name = `[TET] ${itemName}`
+        } else if (item.enhancement === 19) {
+            item.enhancement += 1;
+            item.name = `[PEN] ${itemName}`
+        }
+        return item;
+    },
+    fail: (item) => {
+        const itemName = item.unenhancedName;
+        if (item.type === 'armor' && item.enhancement <= 5) {
+            throw new Error('Armor enhancement is level 5 or under, cannot fail');
+        }
+        if (item.type === 'weapon' && item.enhancement <= 7) {
+            throw new Error('Weapon enhancement is level 7 or under, cannot fail');
+        }
+        if (item.enhancement >= 0 && item.enhancement <= 14) {
+            item.durability -= 5;
+        } else if (item.enhancement > 14 && item.enhancement <= 16) {
+            item.durability -= 10;
+        } else if (item.enhancement === 17) {
+            item.enhancement -= 1;
+            item.durability -= 10;
+            item.name = `[PRI] ${itemName}`
+        } else if (item.enhancement === 18) {
+            item.enhancement -= 1;
+            item.durability -= 10;
+            item.name = `[DUO] ${itemName}`
+        } else if (item.enhancement === 19) {
+            item.enhancement -= 1;
+            item.durability -= 10;
+            item.name = `[TRI] ${itemName}`
+        } else if (item.enhancement === 20) {
+            item.enhancement -= 1;
+            item.durability -= 10;
+            item.name = `[TET] ${itemName}`
+        }
+        return item;
+    },
+    repair: (item) => {
+        item.durability = 100;
+        return item;
+    }
 }
--- index.test.js
+++ index.test.js
@@ -1,28 +1,97 @@
 const index = require('./index');
 
-describe('test outcomes', () => {
+describe('outcomes', () => {
     describe('success outcome', () => {
-        const item = {
-            beforeName: 'test weapon',
-            name: 'test weapon',
+        const weapon1 = {
+            unenhancedName: 'testsword',
+            name: '[TRI]testsword',
+            type: 'weapon',
+            durability: 90,
+            enhancement: 18
+        }
+        const weaponSuccess1 = {
+            unenhancedName: 'testsword',
+            name: '[TET] testsword',
+            type: 'weapon',
+            durability: 90,
+            enhancement: 19
+        }
+        const weapon2 = {
+            unenhancedName: 'testdagger',
+            name: 'testdagger',
+            type: 'weapon',
+            durability: 20,
+            enhancement: 0
+        }
+        const weapon3 = {
+            unenhancedName: 'testsword',
+            name: 'testsword',
+            type: 'weapon',
+            durability: 50,
+            enhancement: 0
+        }
+        const weaponSuccess3 = {
+            unenhancedName: 'testsword',
+            name: '[+1] testsword',
+            type: 'weapon',
+            durability: 50,
+            enhancement: 1
+        }
+        test('Should increase the enhancement of the item by 1', () => {
+            const enhancedItem = index.success(weapon1);
+            const enhancedItem3 = index.success(weapon3);
+            expect(enhancedItem).toEqual(weaponSuccess1);
+            expect(enhancedItem3).toEqual(weaponSuccess3);
+        })
+        test('error if durability is below 25 and item enhancement is less than or equal to 14', () => {
+            expect(() => {
+                index.success(weapon2)
+            }).toThrow();
+        })
+        test('error if item enhancement is already 20', () => {
+            expect(() => {
+                index.success(armor1)
+            }).toThrow();
+        })
+    })
+    describe('fail outcome', () => {
+        const item2 = {
+            unenhancedName: 'testsword',
+            name: '[TRI] testsword',
+            type: 'weapon',
+            durability: 80,
+            enhancement: 18
+        }
+        const failedItem2 = {
+            unenhancedName: 'testsword',
+            name: '[DUO] testsword',
+            type: 'weapon',
+            durability: 70,
+            enhancement: 17
+        }
+        test('Should fail when enhancing', () => {
+            const failedEnhance1 = index.fail(item2);
+            expect(failedEnhance1).toEqual(failedItem2);
+        })
+    })
+    describe('repair outcome', () => {
+        const item1 = {
+            unenhancedName: 'testsword',
+            name: '[TRI] testsword',
+            type: 'weapon',
+            durability: 80,
+            enhancement: 18
+        }
+        const repairedItem1 = {
+            unenhancedName: 'testsword',
+            name: '[TRI] testsword',
             type: 'weapon',
             durability: 100,
-            enhancement: 'PEN'
-        };
-        const expected = {
-            beforeName: 'test weapon',
-            name: '[PEN] test weapon',
-            type: 'weapon',
-            durability: 100,
-            enhancement: 'PEN'
-        };
-        it('should give back same item', () => {
-            const actual = success(item);
-            expect(actual).toEqual(expected)
-        });
-        it('type is weapon or armor', () => {
-            expect(item.type).toBe('weapon' || 'armor')
-        });
-    });
-
-});
+            enhancement: 18
+        }
+        const repairedItem = index.repair(item1);
+        test('Returns item with durability at 100', () => {
+            expect(repairedItem).toEqual(repairedItem1);
+        })
+    })
+})
