2a135b98b5979f35e8aed4a2dc4761ea29720263
Mitchell Robles <mitchellr92@live.com>
2019-04-10 13:07:40 -0700

    basic sturcture set up on enhancer.spec.js

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+// test suite
+describe('the enhancer module', () => {
+
+    // test suite
+    describe('the succeed function', () => {
+
+    });
+
+    // test suite
+    describe('the fail function', () => {
+
+    });
+
+    // test suite
+    describe('the repair function', () => {
+
+    });
+});
