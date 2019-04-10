7b310b75d5158be8575f48ac86bfbcffa6f3b3aa
Kseniya Platonava <ksu2008@gmail.com>
2019-04-10 15:35:24 -0400

    Add Functions and Repare Test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,16 +6,37 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
-}
+  const enhancement = item.enhancement;
+  if (item.enhancement > 19) {
+    return { ...item, enhancement: enhancement };
+  } else {
+    return { ...item, enhancement: enhancement + 1 };
+  }
+}	
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement < 15 && item.durability < 25) {
+    return { ...item };
+  }
+
+   const durability =
+    item.enhancement < 15 ? item.durability - 5 : item.durability - 10;
+  const enhancement =
+    item.enhancement > 16 ? item.enhancement - 1 : item.enhancement;
+
+   return { ...item, durability, enhancement };
+}	
+
+
 
 function repair(item) {
-  return { ...item };
-}
+  if (item.durability !== 100) {
+    return { ...item, durability: 100 };
+  } else {
+    null;
+  }
+}	
+
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('test module', () =>{
+
+  
+  describe('repair test', () =>{
+
+    it('returns durabilty to 100', () => {
+      //arrange
+      const item = {
+        name: 'longbow',
+        type: 7,
+        durability: 54
+      };
+      // act
+       expect(enhancer.repair(item).durability).toEqual(100);
+    });
+  });
+  
+  });
+
+
+
