b830a0dae1c86a0ebdbb96cd2d7df80618d66eb7
Mitchell Robles <mitchellr92@live.com>
2019-04-11 14:54:28 -0700

    Tests have been added and are passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,6 @@
 const enhancer = require('./enhancer.js');
+const { repair, fail, succeed } = require('./enhancer.js');
+
 // test away!
 
 // test suite
@@ -9,22 +11,44 @@ describe('the enhancer module', () => {
 
         // test
         it('items enhancement should increase by an increment of 1', () => {
+            
+            // Arrange
+            const expected = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 14
+            };
 
+            // Act
             const item = {
                 name: "broad sword",
                 durability: 100,
                 enhancement: 13
             };
+
+            // Assert
+            expect(succeed(item)).toEqual(expected);
         });
 
         // test
         it('item with an enhancement of 20 will remain the same', () => {
 
+            // Arrange
+            const expected = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 20
+            }
+
+            // Act
             const item = {
                 name: "broad sword",
                 durability: 100,
                 enhancement: 20
             };
+
+            // Assert
+            expect(succeed(item)).toEqual(expected)
         });
     });
 
@@ -34,39 +58,64 @@ describe('the enhancer module', () => {
         // test
         it('will return item with -5 durability', () => {
 
-            const item = {
-                name: "broad sword",
-                durability: 100,
-                enhancement: 11
-            };
-
+            // Arrange
             const expected = {
                 name: "broad sword",
                 durability: 95,
                 enhancement: 11
             };
 
-            expect(item).toBe(expected);
+            // Act
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 11
+            };
+
+            // Assert
+            expect(fail(item)).toEqual(expected);
         });
 
         // test
         it('will return item with -10 durability', () => {
+            
+            // Arrange
+            const expected = {
+                name: "broad sword",
+                durability: 90,
+                enhancement: 15
+            };
 
+            // Act
             const item = {
                 name: "broad sword",
                 durability: 100,
                 enhancement: 15
             };
+
+            // Assert
+            expect(fail(item)).toEqual(expected);
         });
 
         // test
         it('will return item with -10 durability and -1 enhancement', () => {
 
+            // Arrange
+            const expected = {
+                name: "broad sword",
+                durability: 90,
+                enhancement: 17
+            };
+
+            // Act
             const item = {
                 name: "broad sword",
                 durability: 100,
                 enhancement: 18
             };
+
+            // Assert
+            expect(fail(item)).toEqual(expected);
         });
     });
 
@@ -76,11 +125,22 @@ describe('the enhancer module', () => {
         // test
         it('items durability should return to 100 after it is repaired', () => {
 
+            // Arrange
+            const expected = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 20
+            };
+
+            // Act
             const item = {
                 name: "broad sword",
                 durability: 30,
                 enhancement: 20
             };
+
+            // Assert
+            expect(repair(item)).toEqual(expected);
         });
     });
 });
