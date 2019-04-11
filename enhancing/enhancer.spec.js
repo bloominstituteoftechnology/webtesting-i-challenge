ca36eb4fb77d6df27431935ba5a171276abaab6d
Mitchell Robles <mitchellr92@live.com>
2019-04-10 20:21:01 -0700

    put some place holder items in test suites. Need to finish asserting

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,26 +6,81 @@ describe('the enhancer module', () => {
 
     // test suite
     describe('the succeed function', () => {
-        
-        // test
-        it('item is enhanced', () => {
 
+        // test
+        it('items enhancement should increase by an increment of 1', () => {
+
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 13
+            };
+        });
+
+        // test
+        it('item with an enhancement of 20 will remain the same', () => {
+
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 20
+            };
         });
     });
 
     // test suite
     describe('the fail function', () => {
 
-        it('enhancement failure', () => {
+        // test
+        it('will return item with -5 durability', () => {
 
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 11
+            };
+
+            const expected = {
+                name: "broad sword",
+                durability: 95,
+                enhancement: 11
+            };
+
+            expect(item).toBe(expected);
+        });
+
+        // test
+        it('will return item with -10 durability', () => {
+
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 15
+            };
+        });
+
+        // test
+        it('will return item with -10 durability and -1 enhancement', () => {
+
+            const item = {
+                name: "broad sword",
+                durability: 100,
+                enhancement: 18
+            };
         });
     });
 
     // test suite
     describe('the repair function', () => {
 
-        it('item has been repaired', () => {
+        // test
+        it('items durability should return to 100 after it is repaired', () => {
 
+            const item = {
+                name: "broad sword",
+                durability: 30,
+                enhancement: 20
+            };
         });
     });
 });
