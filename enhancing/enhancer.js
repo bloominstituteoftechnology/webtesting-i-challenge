e6923e2bf7eb661c9c26ada2a0f1297d8d29d9ef
Jesse Theisen <theiseja@gmail.com>
2019-04-10 21:59:50 -0500

    MVP: Implemented get and fail functions and tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,8 +2,21 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
+  get
 };
+
+function fail(item) {
+  if (item.enhancement < 15 && item.durability < 25) {
+    return { ...item };
+  }
+
+  const durability =
+    item.enhancement < 15 ? item.durability - 5 : item.durability - 10;
+  const enhancement =
+    item.enhancement > 16 ? item.enhancement - 1 : item.enhancement;
+
+  return { ...item, durability, enhancement };
+}
 
 function success(item) {
   const enhancement = item.enhancement;
@@ -12,10 +25,6 @@ function success(item) {
   } else {
     return { ...item, enhancement: enhancement + 1 };
   }
-};
-
-function fail(item) {
-  return { ...item };
 }
 
 function repair(item) {
@@ -27,5 +36,7 @@ function repair(item) {
 }
 
 function get(item) {
-  return { ...item };
+  const name =
+    item.enhancement < 1 ? item.name : item.name + [item.enhancement];
+  return { ...item, name };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,34 +1,90 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require('./enhancer');
+const { repair } = require('./enhancer');
 
-describe('repair()', () => {
+describe('enhancer', () => {
+  describe('fail()', () => {
+    it('durability decreases by 5 if enhancement is between 0 and 14', () => {
+      const item = {
+        enhancement: 14,
+        durability: 55
+      };
+      const actual = enhancer.fail(item);
+      expect(actual.durability).toBe(50);
+    });
+
+    it('durability decreases by 10 if enhancement is greater than 14', () => {
+      const item = {
+        enhancement: 15,
+        durability: 55
+      };
+
+      const actual = enhancer.fail(item);
+
+      expect(actual.durability).toBe(45);
+    });
+
+    it('If enhancement is 14 or lower, the item cannot be enhanced if the durability is below 25', () => {
+      const item = {
+        enhancement: 14,
+        durability: 24
+      };
+
+      expect(enhancer.fail(item)).toEqual(item);
+    });
+
+    it('should decrease enhancement if item is enhanced greater than 16', () => {
+      const item = {
+        enhancement: 18
+      };
+
+      const actual = enhancer.fail(item);
+
+      expect(actual.enhancement).toBe(17);
+    });
+  });
+
+  describe('success()', () => {
+    it('Adds enhancement by +1', () => {
+      const item = {
+        enhancement: 16
+      };
+
+      const actual = enhancer.success(item);
+
+      expect(actual.enhancement).toBe(17);
+    });
+
+    it('Enhancement above 19 does not get increased', () => {
+      const item = {
+        enhancement: 20
+      };
+
+      const actual = enhancer.success(item);
+
+      expect(actual.enhancement).toBe(20);
+    });
+  });
+  describe('repair()', () => {
     it('returns durabilty to 100', () => {
       const item = {
         name: 'Gladius',
         type: 'sword',
         durability: 98
       };
-      const repair = enhancer.repair(item);
-      expect(repair.durability).toBe(100);
+
+      expect(repair(item).durability).toEqual(100);
     });
   });
-describe('success()', () => {
-    it('Adds enhancement by +1', () => {
-        const item = {
-            enhancement: 16
-        };
-        const actual = enhancer.success(item);
 
-        expect(actual.enhancement).toBe(17);
+  describe('get()', () => {
+    it('returns a moded name', () => {
+      const item = {
+        name: 'Gladius',
+        type: 'sword',
+        enhancement: 0
+      };
+
+      expect(enhancer.get(item).name).toEqual('Gladius');
     });
-
-    it('Enhancement above 19 does not get increased', () => {
-        const item = {
-            enhancement: 20
-        };
-
-        const actual = enhancer.success(item);
-
-        expect(actual.enhancement).toBe(20);
-    });
+  });
 });
