e89ee4ac2d965910de567d0782a79c3b34fe0a85
Patrick <patrick.schwindt14@gmail.com>
2019-04-15 13:07:41 -0600

    Commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,8 +5,24 @@ module.exports = {
   get,
 };
 
+const item = {name: '', durability: '', enhancement: ''}
+
 function succeed(item) {
+  if (item !== 20){
+  item = {
+    ...item,
+    enhancement: item + 1,
+    durability: item
+  }
   return { ...item };
+}else{
+  item = {
+    ...item,
+    enhancement: item,
+    durability: item
+  };
+  return{...item}
+}
 }
 
 function fail(item) {
@@ -14,9 +30,14 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  item = {
+    ...item,
+    durability: 100
+  }
+  return {...item};
 }
 
 function get(item) {
+
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
-const enhancer = require('./enhancer.js');
+const { succeed, repair, fail, get } = require('./enhancer.js');
 // test away!
+
+//accepts an item object and returns a new item with the durability restored to 100
+//Repair
+it('accepts an item object and returns a new item with the durability restored to 100', () =>{
+    expect (repair(20).durability).toBe(100);
+    expect (repair(99).durability).toBe(100);
+    expect (repair(0).durability).toBe(100);
+})
+
+//Succeed
+it('Adheres to MVP test conditions of Success', () =>{
+    //Enhancement increases by one?
+    expect(succeed(1).enhancement).toBe(2);
+    //if enhancement is 20, level is not changed
+    expect(succeed(20).enhancement).toBe(20);
+    //Durability isn't changed
+    expect(succeed(40).durability).toBe(40);
+    
+
+})
