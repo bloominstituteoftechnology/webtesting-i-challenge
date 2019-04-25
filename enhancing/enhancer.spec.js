a25c253cbe66b76faed8041cb6cf945b56460b63
Mitchell Robles <mitchellr92@live.com>
2019-04-24 19:20:22 -0700

    Created helper fucntion to test items, created succeed fucntion tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,8 +3,31 @@ const enhancer = require('./enhancer.js');
 
 describe('enhancer module', () => {
 
+    const makeSword = (enhancement = 0, durability = 100) => ({
+        name: 'Broad Sword',
+        durability,
+        enhancement
+    });
+
     describe('succeed function', () => {
 
+        it('Items enhancement should increase by an increment of 1', () => {
+
+            const expected = makeSword(enhancement = 14);
+            const item = makeSword(enhancement = 13);
+
+            expect(succeed(item)).toBe(expected);
+
+        });
+
+        it('Item with an enhancement of 20 will remain the same', () => {
+
+            const expected = makeSword(enhancement = 20);
+            const item = makeSword(enhancement = 20);
+
+            expect(succeed(item)).toBe(expected);
+
+        });
     });
 
     describe('fail function', () => {
