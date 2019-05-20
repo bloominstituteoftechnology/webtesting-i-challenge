5475db4818fef38729b11632831f3c67a2581c97
sriley <sarahriley.dev@gmail.com>
2019-05-20 18:17:23 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,40 +6,51 @@ module.exports = {
 };
 
 function succeed(item) {
-  if(item.enhancement < 20 ){
+  if (item.enhancement < 20) {
     const newItem = {
       name: item.name,
       durability: 100,
       enhancement: item.enhancement + 1
-    }
+    };
     return newItem;
-  }else {
+  } else {
     return item;
   }
-
 }
 
 function fail(item) {
-  if(item.enhancement < 15){
+  if (item.enhancement < 15) {
     const newItem = {
       name: item.name,
       durability: item.durability - 5,
       enhancement: item.enhancement
-    }
+    };
     return newItem;
-  }else {
-    return item;
+  } else if (item.enhancement > 16) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement -1
+    };
+    return newItem;
+  } else if (item.enhancement > 14) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement 
+    };
+    return newItem;
   }
+  return item;
 }
 
 function repair(item) {
- 
-    const newItem = {
-      name: item.name,
-      durability: 100,
-      enhancement: item.enhancement
-    };
-    return newItem;
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,5 @@
-const {repair, succeed, fail, get} = require("./enhancer.js");
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
-
-
 
 describe("enhancer.js", () => {
   describe("repair()", () => {
@@ -10,7 +8,7 @@ describe("enhancer.js", () => {
         name: "Sarah",
         durability: 10,
         enhancement: 15
-      }
+      };
       //arrange
       const expected = {
         name: "Sarah",
@@ -26,41 +24,74 @@ describe("enhancer.js", () => {
     });
   });
 
-  describe('succeed()', () => {
+  describe("succeed()", () => {
     it("should raise enhancement by 1", () => {
-     
       const item = {
         name: "Sarah",
         durability: 10,
         enhancement: 15
-      }
+      };
       const expected = {
         name: "Sarah",
         durability: 100,
         enhancement: 16
-      }
+      };
 
       const enhancedItem = succeed(item);
       expect(enhancedItem).toEqual(expected);
-    })
-  })
+    });
+  });
 
-  describe('fail()', () => {
-    it("decrease enhancement if fails", () => {
+  describe("fail()", () => {
+    it("decrease durability by 5 if less than 15", () => {
       const item = {
         name: "Sarah",
         durability: 50,
         enhancement: 14
-      }
+      };
 
       const expected = {
         name: "Sarah",
         durability: 45,
         enhancement: 14
-      }
+      };
 
       const failItem = fail(item);
       expect(failItem).toEqual(expected);
-    })
-  })
+    });
+
+    it("decrease durability by 10 if greater than 14", () => {
+      const item = {
+        name: "Sarah",
+        durability: 50,
+        enhancement: 16
+      };
+
+      const expected = {
+        name: "Sarah",
+        durability: 40,
+        enhancement: 16
+      };
+
+      const failItem = fail(item);
+      expect(failItem).toEqual(expected);
+    });
+
+    it("decrease durability by 1 if greater than 16", () => {
+      const item = {
+        name: "Sarah",
+        durability: 50,
+        enhancement: 18
+      };
+
+      const expected = {
+        name: "Sarah",
+        durability: 40,
+        enhancement: 17
+      };
+
+      const failItem = fail(item);
+      expect(failItem).toEqual(expected);
+    });
+  });
 });
