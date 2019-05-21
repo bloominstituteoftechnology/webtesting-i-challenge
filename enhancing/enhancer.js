edff1a992baed769867a650062c1a08adb1695a8
jkaddu <jkaddu@yahoo.com>
2019-05-20 21:03:55 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,46 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	const enhancement = item.enhancement;
+	if (enhancement < 20) {
+		return { ...item, enhancement: enhancement + 1 };
+	} else {
+		return { ...item };
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	// grab the property wanted to change
+	let durability = item.durability;
+	let enhancement = item.enhancement;
+
+	// created an if/else statement without returns to capture all the changes
+	if (enhancement < 15) {
+		durability -= 5;
+	} else {
+		durability -= 10;
+	}
+	if (enhancement > 16) {
+		enhancement -= 1;
+	}
+	// returning the new object with changes to both enhancement and durability
+	return { ...item, enhancement: enhancement, durability: durability };
 }
 
 function repair(item) {
-  return { ...item };
+	const durability = item.durability;
+	if (durability < 100) {
+		return { ...item, durability: 100 };
+	} else {
+		return { ...item };
+	}
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,16 +1,47 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
+// created object for test
+const myItem = {
+	name: 'hammer',
+	durability: 85,
+	enhancement: 10
+};
+
+const myItemAtEnhancement20 = {
+	name: 'hammer',
+	durability: 85,
+	enhancement: 20
+};
+
 describe('enhancer.js', () => {
 	describe('repair() method', () => {
 		it('should return a durability of 100', () => {
-			expect(enhancer.durability()).toBe(100);
+			// passing in myItem into the repair method and storing it in newItemObject.
+			const newItemObject = enhancer.repair(myItem);
+
+			expect(newItemObject.durability).toBe(100);
+			// below you can call a method/property immediately from a function
+			// expect(enhancer.repair(myItem).durability).toBe(100);
 		});
-		// it('should return a durability of 100', () => {
-		// 	expect(enhancer.durability()).toBe();
-		// });
-		// it('should return a durability of 100', () => {
-		// 	expect(enhancer.durability()).toBe();
-		// });
+		it('should return a enhancement of +1 with succeed', () => {
+			// expected result with succeed is enhancement + 1
+			let expected = myItem.enhancement + 1;
+			// if enhancement is more than 20 set it to 20
+			if (expected > 20) {
+				expected = 20;
+			}
+			expect(enhancer.succeed(myItem).enhancement).toBe(expected);
+			expect(enhancer.succeed(myItemAtEnhancement20).enhancement).toBe(20);
+		});
+		it('should return a durability decreased by 5 when enhancement is < 15', () => {
+			expect(enhancer.fail(myItem).durability).toBe(80);
+		});
+		it('should return a durability decreased by 10 when enhancement is >= 15', () => {
+			expect(enhancer.fail(myItemAtEnhancement20).durability).toBe(75);
+		});
+		it('should return a enhancement decreased by 1 when enhancement is > 16', () => {
+			expect(enhancer.fail(myItemAtEnhancement20).enhancement).toBe(19);
+		});
 	});
 });
