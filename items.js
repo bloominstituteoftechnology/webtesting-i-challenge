e5b7ad939b6c44721bce20331cc9f6009504b0c8
Trip1701 <bj2336@email.vccs.edu>
2019-06-19 09:51:22 -0400

    Items Array Created in items.js

---
+++ items.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        name: "Buster Sword I",
+        durability: 97,
+        enhancement: 16
+    },
+    {
+        name: "Buster Sword II",
+        durability: 0,
+        enhancement: 16
+    },
+    {
+        name: "Buster Sword III",
+        durability: 97,
+        enhancement: 0
+    },
+]
