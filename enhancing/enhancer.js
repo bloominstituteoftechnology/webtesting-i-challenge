3b30aa433877b5bdc0ff30b83eefe7dadf1e3386
James Wigdor <jpwigdor@gmail.com>
2019-06-19 23:21:05 -0400

    Completed tests for REPAIR

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,7 +2,7 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
@@ -14,9 +14,12 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  item = { ...item, durability: 100 };
+  return item;
 }
 
 function get(item) {
   return { ...item };
 }
+
+//red, green refactor, commit
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,30 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer.js", function() {
+  describe("repair()", () => {
+    it("restores durability to 100", () => {
+      const item = {
+        durability: 130,
+        enahncement: 3
+      };
+
+      expect(enhancer.repair({ durability: 89 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: -89 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: 100 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: 0 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: 0 })).toEqual({ durability: 100 });
+    });
+  });
+
+  it.todo("should have a max of 100 for durability");
+});
+
+function makeItem(name, durability) {
+  return {
+    name,
+    durability
+  };
+}
+
+// enhancer.repair should make durability 100
