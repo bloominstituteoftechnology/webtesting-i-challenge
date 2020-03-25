02cb71d88281280fd7f76d5002cef6fc3f8d2de5
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-03-25 14:39:01 -0700

    Make enhancer functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,34 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
+function succeed({name, enhancement, durability}) {
+  return {
+    name,
+    enhancement: Math.min(enhancement, 20),
+    durability
+  };
 }
 
-function fail(item) {
-  return { ...item };
+function fail({name, enhancement, durability}) {
+  return {
+    name,
+    enhancement: enhancement > 16 ? enhancement-1 : enhancement,
+    durability: Math.max(durability - (enhancement < 15 ? 5 : 10), 0)
+  };
 }
 
-function repair(item) {
-  return { ...item };
+function repair({name, enhancement, durability}) {
+  return {
+    name,
+    enhancement,
+    durability: 100
+  };
 }
 
-function get(item) {
-  return { ...item };
+function get({name, enhancement, durability}) {
+  return {
+    name: enhancement === 0 ? name : `[+${enhancement}] ${name}`,
+    enhancement,
+    durability
+  };
 }
