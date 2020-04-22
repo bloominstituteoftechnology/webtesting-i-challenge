951a078752407c2a7085c33194a68151e582e78f
Harrison <hmseaborn@gmail.com>
2020-04-22 13:58:53 -0700

    i mean test are passing idk
    one step at a time i guess

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -16,7 +16,12 @@ describe('enhancer', () => {
     })
   })
   describe('items enhancement number is in fact a number and between 0 and 20 oooof', () => {
-    it.todo('items enhancement value is indeed a number instead of idk a string or empty objects and arrays or even just inside an array maybe who knows.... test')
+    it('items enhancement value is indeed a number instead of idk a string or empty objects and arrays or even just inside an array maybe who knows.... test', () => {
+      const test1 = { enhancement: 5 }
+      expect(typeof test1.enhancement).toBe('number')
+      const test2 = { enhancement: '5' }
+      expect(typeof test2.enhancement).not.toBe('number')
+    })
     it.todo('oof check to see if it is between0 and 20')
   })
 })
