79e99870a89768463e44641d83b8a9bb1d067edc
Harrison <hmseaborn@gmail.com>
2020-04-22 12:42:41 -0700

    got a test up checking to expect name for item

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail, get } = require('./enhancer.js');
 // test away!
+describe('enhancer', () => {
+  describe('check to see if items contain name durab and enhancement values', () => {
+    it('has a name value', () => {
+      const test = { name: 'daggers' }
+      expect(test).toHaveProperty('name')
+    })
+  })
+})
