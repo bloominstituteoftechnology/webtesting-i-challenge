0d7a762e9868969aeaf5936af95b483c05b5b293
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-04-28 22:23:54 -0700

    Completed MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,41 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const {enhancement} = item;
+  if(enhancement == 20 ){
+    return { ...item }
+  }else if(enhancement < 20 && enhancement > -1){
+      return{ ...item, enhancement: enhancement +1 };
+  }else {
+    return "Item does not exist"
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const {enhancement, durability} = item;
+  if((typeof(enhancement, durability)!== 'number') || Number.isNaN(enhancement, durability)){
+    throw new Error('Invalid argument types')
+  } else if(enhancement < 15 && enhancement > -1 ){
+    return { ... item, durability:durability -5}
+
+  }else if(enhancement == 15 || enhancement == 16 && durability > 10 && durability <101){
+    return { ... item, durability: durability -10}
+
+  }else if (enhancement > 16 && enhancement < 21 && durability > 10 && durability <101){
+    return { ...item, enhancement: enhancement -1, durability: durability -10 }
+
+  }else {
+    return "Item does not exist"
+  }
 }
 
 function repair(item) {
-  return { ...item };
+ const { enhancement, durability } = item;
+  if (enhancement > 20|| enhancement < 0 || durability > 100 || durability < 0) {
+    return "Item can't exist"
+  }else{
+    return { ...item, durability: 100 };
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,50 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair, succeed, fail, get } = require('./enhancer.js');
+
+describe('repair function', () => {
+    describe('should return durability equal to 100', () => {
+        it('returns durability 100', () => {
+            expect(repair({durability:12 , enhancement:11})).toEqual({durability: 100, enhancement:11});
+        });
+    });
+});
+
+describe('success function', () => {
+    describe('should increase by one', () => {
+        it('increases enhancement by one' , () => {
+            expect(succeed({enhancement:20})).toEqual({enhancement:20});
+        });
+    });
+});
+
+describe('fail function', () => {
+    describe('should decrease durability by 5', () => {
+        it('decreases durability by 5 ', () => {
+            expect(fail({durability:10, enhancement: 14 })).toEqual({durability: 5, enhancement: 14})
+        })
+    });
+    describe('should decrease durability by 10', () => {
+        it('decreases durability by 10', () => {
+            expect(fail({durability:90, enhancement:15 })).toEqual({ durability:80, enhancement:15})
+        })
+    });
+    describe('should decrease enhancement by 1', () => {
+        it('decreases enhancement by 1', () => {
+            expect(fail({ enhancement:19, durability:20 })).toEqual({  enhancement: 18,  durability: 10})
+        })
+    });
+
+    describe('enhancement and durability should be numbers', () => {
+        it('should return error if enhancement  and durability is not the type of number', () => {
+            expect(() => fail({enhancement:"should be number", durability:"durability too"})).toThrow()
+        })
+    });
+    describe('enhancement and durability should be natural numbers', () => {
+        it.todo('should return error if enhancement and durability are not natural numbers')
+    });
+
+    // describe('', () => {
+    //     it('', () => {
+    //         expect(fail({ })).toEqual({ })
+    //     })
+    // });
+})
