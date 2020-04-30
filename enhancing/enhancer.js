380a32613f644f5dfd7862812e0e9a671817249e
Shya Biswas <shyabiswas@gmail.com>
2020-04-30 17:47:01 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,27 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return item.enhancement < 20
+  ? { ...item, enhancement: item.enhancement + 1 }
+  : {...item};
 }
 
 function fail(item) {
-  return { ...item };
+  const {enhancement, durability} = item;
+  return enhancement > 16
+  ? {...item, enhancement: enhancement-1,
+  durability: durability-1}: enhancement >= 15
+  ? {...item, durability: durability -10}
+  :{ ...item, durability: durability -5};
 }
 
 function repair(item) {
-  return { ...item };
+  return item.durability === 100
+  ?{ ...item }: {...item, durability: 100};
 }
 
 function get(item) {
-  return { ...item };
+  return item.enhancement === 0 
+ ? { ...item }
+ :{ ...item, name: `[+${item.enhancement}] ${item.name}`};
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
+const {fail, get, repair, succeed} = require('./enhancer.js');
 // test away!
+describe( "enhancer.js", ()=>{
+    describe("fail()", () => {
+        it("returns a new item with durability decreased by 5", ()=> {
+            expect(fail({ name: "failure", enhancement: 14, durability: 100})).toEqual({
+                name: "failure", enhancement:14, durability:95
+            });
+        });
+    it("returns a new item with durability decreased by 10", ()=>{
+        expect(fail({name: "fail1", enhancement: 15, durability: 100})).toEqual({
+            name: "fail1",
+            enhancement: 15,
+            durability:90
+        });
+    });
+    it ("returns a new item with enhancement decreased by 1 and durability decreased by 1", ()=>{
+        expect (
+            fail({ name: "failure2", enhancement: 17, durability: 100})
+        ).toEqual({ name: "failure2", enhancement:16, durability: 99});
+    });
+});
+
+describe("get()", ()=>{
+    it("returns a new item with no modification if enhancement is 0", ()=>{
+        expect(get({ name: "checkmark", enhancement: 0, durability:10})).toEqual({
+            name: "checkmark",
+            enhancement: 0,
+            durability: 10
+        });
+    });
+});
+describe("succeed()", ()=>{
+    it("returns new item with same enhancement value", ()=>{
+        expect({ name: "Success", enhancement: 20, durability:100}).toEqual({
+            name: "Success",
+            enhancement:20,
+            durability: 100
+        });
+    });
+    it("returns new item with enhancement value + 1", ()=>{
+        expect(succeed({ name: "supersuccess", enhancement: 15, durability: 100})).toEqual({
+            name: "supersuccess",
+            enhancement: 16,
+            durability: 100
+        });
+    });
+});
+});
