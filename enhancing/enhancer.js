78c408827a9a77869d79f2c3183e037b14aa98b1
Mohd Raza <noorinfotechinc@gmail.com>
2020-04-30 10:10:44 -0500

    completed with stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,64 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const { enhancement } = item;
+
+  if (item.enhancement >= 0 && item.enhancement < 20) {
+    const newEnhancement = enhancement + 1;
+    return { ...item, enhancement: newEnhancement };
+  } else if (item.enhancement === 20) {
+    return { ...item, enhancement: item.enhancement };
+  } else {
+    return "enhancement level passed should be less than 20";
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  let newDurability = item.durability;
+  let newEnhancement = item.enhancement;
+  if (item.enhancement >= 0 && item.enhancement < 15) {
+    newDurability = item.durability - 5;
+    return { ...item, durability: newDurability };
+  } else if (item.enhancement >= 15 && item.enhancement <= 20) {
+    if (item.enhancement <= 16) {
+      newDurability = item.durability - 10;
+      return { ...item, durability: newDurability };
+    } else {
+      newDurability = item.durability - 10;
+      newEnhancement = item.enhancement - 1;
+      return {
+        ...item,
+        durability: newDurability,
+        enhancement: newEnhancement,
+      };
+    }
+  } else {
+    return "please correct item parameters passed in";
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (
+    item.durability <= 100 &&
+    item.durability >= 0 &&
+    item.enhancement >= 0 &&
+    item.enhancement <= 20
+  ) {
+    return { ...item, durability: 100 };
+  } else {
+    return "item with wrong parameters passed";
+  }
 }
 
 function get(item) {
-  return { ...item };
+  let newName = item.name;
+  if (item.enhancement > 0 && item.enhancement <= 20) {
+    newName = `[+${item.enhancement}] ${item.name}`;
+    console.log("new name: ", newName);
+    return { ...item, name: newName };
+  } else if (item.enhancement === 0) {
+    return { ...item };
+  } else {
+    return "please pass item with correct parameters";
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,148 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("a repair(item) method that accepts an item object and returns a new item with the durability restored to 100", () => {
+  it("should accept an object and return 100 durability", () => {
+    expect(
+      enhancer.repair({
+        name: "Item1",
+        durability: 0,
+        enhancement: 18,
+      })
+    ).toEqual({
+      name: "Item1",
+      enhancement: 18,
+      durability: 100,
+    });
+  });
+
+  it("should accept an object, ensures the returned value is 100, and not anythign else", () => {
+    expect(
+      enhancer.repair({
+        name: "Item1",
+        durability: 0,
+        enhancement: 18,
+      })
+    ).not.toEqual({
+      name: "Item1",
+      enhancement: 18,
+      durability: 98,
+    });
+  });
+});
+
+describe("a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success", () => {
+  it("should return item's enhancement increases by 1.", () => {
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 1,
+      })
+    ).toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 2,
+    });
+  });
+
+  it("should return ehancement level 20 if passed in 20", () => {
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 20,
+      })
+    ).toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 20,
+    });
+
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 20,
+      })
+    ).not.toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 21,
+    });
+  });
+});
+
+describe("a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.", () => {
+  it("should decrease durability by 5 if enhancement is less than 15", () => {
+    expect(
+      enhancer.fail({
+        name: "item3",
+        durability: 90,
+        enhancement: 10,
+      })
+    ).toEqual({
+      name: "item3",
+      durability: 85,
+      enhancement: 10,
+    });
+  });
+
+  it("should decrease durability by 10 if enhancement is 15 or more", () => {
+    expect(
+      enhancer.fail({
+        name: "item3",
+        durability: 80,
+        enhancement: 16,
+      })
+    ).toEqual({
+      name: "item3",
+      durability: 70,
+      enhancement: 16,
+    });
+  });
+
+  it("should decrease durability by 10 and decrease enhancement by 1 if enhancement is greater than 16 or more", () => {
+    expect(
+      enhancer.fail({
+        name: "item3",
+        durability: 80,
+        enhancement: 20,
+      })
+    ).toEqual({
+      name: "item3",
+      durability: 70,
+      enhancement: 19,
+    });
+  });
+});
+
+describe("Add a get() method to the enhancer object that takes an item and returns a new item with the name property modified according to the following rules:", () => {
+  it("if the enhancement level is greater than 0, change the name to include the enhancement level, preceded by a plus sign ( + ), between square brackets before the item's name. Example: the name of a Iron Sword enhanced to 7 would be [+7] Iron Sword ", () => {
+    expect(
+      enhancer.get({
+        name: "item1",
+        durability: 10,
+        enhancement: 7,
+      })
+    ).toEqual({
+      name: "[+7] item1",
+      durability: 10,
+      enhancement: 7,
+    });
+  });
+
+  it("if the enhancement level is 0, the the name is not modified.", () => {
+    expect(
+      enhancer.get({
+        name: "item1",
+        durability: 10,
+        enhancement: 0,
+      })
+    ).toEqual({
+      name: "item1",
+      durability: 10,
+      enhancement: 0,
+    });
+  });
+});
