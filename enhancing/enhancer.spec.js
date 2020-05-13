af084e3217248b24017188b897373fba95418145
Michael Lowe <mrlowememphis@yahoo.com>
2020-05-13 09:00:29 -0500

    success item added to enhancer.spec test working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -30,3 +30,45 @@ describe("a repair(item) method that accepts an item object and returns a new it
     });
   });
 });
+
+describe("a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success", () => {
+    it("should return item's enhancement increases by 1.", () => {
+      expect(
+        enhancer.succeed({
+          name: "Item2",
+          durability: 0,
+          enhancement: 1,
+        })
+      ).toEqual({
+        name: "Item2",
+        durability: 0,
+        enhancement: 2,
+      });
+    });
+  
+    it("should return ehancement level 20 if passed in 20", () => {
+      expect(
+        enhancer.succeed({
+          name: "Item2",
+          durability: 0,
+          enhancement: 20,
+        })
+      ).toEqual({
+        name: "Item2",
+        durability: 0,
+        enhancement: 20,
+      });
+  
+      expect(
+        enhancer.succeed({
+          name: "Item2",
+          durability: 0,
+          enhancement: 20,
+        })
+      ).not.toEqual({
+        name: "Item2",
+        durability: 0,
+        enhancement: 21,
+      });
+    });
+  });
