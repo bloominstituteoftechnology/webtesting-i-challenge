acfd69b16362bfe27cf5cdbb2761da2bf826540b
John Daly <jcdaly97@gmail.com>
2020-05-20 14:30:30 -0700

    hit stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,55 @@ module.exports = {
 };
 
 function succeed(item) {
+  if(item.enhancement < 20 && item.enhancement > 0){
+    item.enhancement ++
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if(item.enhancement < 15){
+    item.durability -= 5
+  }else if(item.enhancement > 16){
+    item.enhancement --
+    item.durability -= 10
+  }else{
+    item.durability -= 10
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100
   return { ...item };
 }
 
 function get(item) {
+  newName = `[+${item.enhancement}] ${item.name}`
+  item.name = newName
   return { ...item };
+}
+
+function validateItem(item){
+  //checks if the proper keys exist
+  if(item.name && item.durability && item.enhancement){
+    //the checks that the values are the proper datatype
+    if(typeof item.name === 'string' && typeof item.durability === 'number' && typeof item.enhancement === 'number'){
+      //then checks that the number values are in the proper range
+      if(item.durabilty <= 100 && item.durability > 0){
+        if(item.enhancement <= 20 && item.enhancement >= 0){
+          return true;
+        }else{
+          console.log('the enhancement is out of the propper range')
+          return false;
+        }
+      }else{
+        console.log()
+      }
+    }
+    
+  }else{
+    console.log('you need the correct parameters')
+    return false;
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,64 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const testItem1= {
+    name: 'iron sword of testing',
+    durability: 75,
+    enhancement: 3
+}
+const testItem2= {
+    name: 'steel sword of testing',
+    durability: 50,
+    enhancement: 15
+}
+const testItem3= {
+    name: 'diamond sword of testing',
+    durability: 60,
+    enhancement: 18
+}
+
+
+describe('repair', ()=>{
+    it('should return item with durability of 100',
+    ()=>{
+        expect(()=>{
+            enhancer.repair(testItem1).toBe(testItem1.durability==100)
+        })
+    })
+})
+
+describe('enhancer',()=>{
+    describe('success', ()=>{ 
+        it('successful enhancement',
+        ()=>{
+            expect(()=>{
+                originalValue = testItem1.enhancement
+                enhancer.succeed(testItem1).toBe(testItem1.enhancement == (originalValue + 1))
+            })
+
+        })
+    })
+    describe('failure cases',()=>{
+        it('failure case 1', ()=>{
+            expect(()=>{
+                originalValue = testItem1.durability
+                enhancer.fail(testItem1).toBe(testItem1.durability == (originalValue - 5))
+            })
+        })
+        it('failure case 2', ()=>{
+            expect(()=>{
+                originalValue = testItem2.durability
+                enhancer.fail(testItem2).toBe(testItem2.durability == (originalValue - 10))
+            })
+        })
+        it('failure case 3', ()=>{
+            expect(()=>{
+                originalValue1 = testItem3.durability
+                originalValue2 = testItem3.enhancement
+                enhancer.fail(testItem3).toBe(testItem3.durability == (originalValue - 10) && testItem3.enhancement == (originalValue2 - 1))
+
+            })
+        })
+    })
+    
+})
