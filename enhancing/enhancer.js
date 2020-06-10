3b119fea2fd3c8ba15c60a44311fcc84f4a3c4ce
thomas <t_huynh927@yahoo.com>
2020-06-09 20:29:36 -0700

    first commit, finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5
+  } else {
+    item.durability = item.durability - 10
+  }
+  if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,69 @@
-const enhancer = require('./enhancer.js');
+const {
+    succeed,
+    fail,
+    repair,
+    get,
+  } = require('./enhancer.js');
 // test away!
+
+
+describe("enhancer", () => {
+    describe("repair", () => {
+        it("should restore durability to 100", () => {
+            let item = {
+                name: "Buster sword",
+                durability: 27,
+                enhancement: 10
+            }
+            const repairedItem = repair(item)
+
+            expect(repairedItem.durability).toBe(100)
+        })
+    })
+
+    describe("succeed", () => {
+        it("should increase enhancement by 1", () => {
+            let enhancementStat = 10
+            let item = {
+                name: "Buster sword",
+                durability: 27,
+                enhancement: enhancementStat
+            }
+            const enhancedItem = succeed(item)
+            expect(enhancedItem.enhancement).toBe(enhancementStat + 1)
+        })
+        it("should have enhancement stay at 20", () => {
+            let item = {
+                name: "Buster sword",
+                durability: 27,
+                enhancement: 20
+            }
+            const enhancedItem = succeed(item)
+            expect(enhancedItem.enhancement).toBe(20)
+        })
+    })
+    describe("fail", () => {
+        it("should decrease durability by 5", () => {
+            let durabilityStat = 27
+            let item = {
+                name: "Buster sword",
+                durability: durabilityStat,
+                enhancement: 14
+            }
+            const failedItem = fail(item)
+            expect(failedItem.durability).toBe(durabilityStat - 5)
+        })
+        it("should decrease durability by 10 and decrease enchancement by 1", () => {
+            let durabilityStat = 27
+            let enchancementStat = 17
+            let item = {
+                name: "Buster sword",
+                durability: durabilityStat,
+                enhancement: enchancementStat
+            }
+            const failedItem = fail(item)
+            expect(failedItem.durability).toBe(durabilityStat - 10)
+            expect(failedItem.enhancement).toBe(enchancementStat - 1)
+        })
+    })
+})
