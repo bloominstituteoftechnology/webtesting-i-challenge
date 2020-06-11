13774cb94278e713fdffe0ccf5ccbd0fc3cab961
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-06-12 01:21:40 +0800

    initialized project, completed one test on repair()

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,19 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  return { ...item, item };
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item, item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair } = require("./enhancer.js");
 // test away!
+
+describe("enhancer functionality", () => {
+  describe("repair()", () => {
+    it("returns a new item with durability restored to 100", () => {
+      expect(repair({ name: "yen", durability: 50, enhancement: 10 })).toEqual({
+        name: "yen",
+        durability: 100,
+        enhancement: 10
+      });
+    });
+  });
+});
