fda4eb929c0c0cca765f971f8a391a551ac2fd6d
berdiyeva <berdiyevaaltyn@gmail.com>
2020-07-16 17:19:35 -0500

    mvp is not finished yet

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,25 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get,
 };
 
 function succeed(item) {
-  return { ...item };
+	const result = {};
+	return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+	const result = {};
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+	const result = {};
+	return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer unit test", () => {
+	it("repair", () => {
+		expect(enhancer.repair()).toBe();
+	});
+
+	it("success", () => {
+		expect(calculator.succeed()).toBe();
+	});
+
+	it("fail", () => {
+		expect(calculator.fail()).toBe();
+	});
+
+	it("get", () => {
+		expect(calculator.get()).toBe();
+	});
+});
