b8bd7f76408336e40b20e0ddd17c64e19ad3042f
ferror18 <ferror18@gmail.com>
2020-07-22 14:33:47 -0700

    Mvp Done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,12 +5,22 @@ module.exports = {
   get,
 };
 
-function success(item) {
-  return { ...item };
+const isRequired = () => { throw new Error('An item is required'); };
+
+function success(item = isRequired) {
+  return { ...item, enhancement: (item.enhancement === 20? item.enhancement++ : item.enhancement)};
 }
 
 function fail(item) {
-  return { ...item };
+  const newItem = {...item}
+  if (item.enhancement < 15) {
+    newItem.durability -= 5
+  }if (item.enhancement >= 15) {
+    newItem.durability -= 10
+  }if (item.enhancement > 16) {
+    newItem.enhancement--
+  }
+  return newItem;
 }
 
 function repair(item) {
@@ -20,3 +30,12 @@ function repair(item) {
 function get(item) {
   return { ...item };
 }
+
+// const prefectItem = {
+//   name: 'Sword of the Devil King',
+//   enhancement: 20,
+//   durability: 85
+// };
+// const prefectItemMaxEnhance = {...prefectItem, enhancement: 20}
+// console.log(success(prefectItem));
+// console.log(success(prefectItemMaxEnhance));
