28fee37229dc7e7fcff0c181e42b57b666a14060
Frytz <dacook1983@gmail.com>
2020-08-11 21:36:11 -0400

    init

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
+const { TestScheduler } = require('jest');
 // test away!
+describe('enhanced items tests', () => {
+
+test.todo('success');
+test.todo('failed');
+test.todo('repair');
+test.todo('get');
+})
