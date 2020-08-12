37e3729d7d59f3969c77bd73550cc9681a0e8684
Nathan Brown <scottynate05@yahoo.com>
2020-08-12 00:50:42 -0700

    tests complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,31 @@ module.exports = {
 };
 
 function success(item) {
+  item.enhancement !== 20 ? item.enhancement++ : item.enhancement;
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.enhancement -= 5;
+  } else if (item.enhancement > 15 && item.enhancement <= 16) {
+    item.enhancement -= 10;
+  } else if (item.enhancement > 16) {
+    item.enhancement--;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
+  if (item.enhancement > 0) {
+    item.name = `[+${item.enhancement}] ${item.name}`;
+  }
+
   return { ...item };
 }
