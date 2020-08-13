ae80b7b075bb2d2cd171162fa3ee3098210b7ec9
Frytz <dacook1983@gmail.com>
2020-08-12 21:08:15 -0400

    completeted

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,43 @@
 module.exports = {
-  success,
+  succeed,
   fail,
   repair,
   get,
 };
 
-function success(item) {
-  return { ...item };
+
+function succeed(item) {
+  const { enhancement } = item;
+  if(enhancement < 20) {
+    item.enhancement += 1;
+  }
+  return { ...item};
 }
 
 function fail(item) {
+  const { enhancement } = item;
+  if(enhancement > 16) {
+    item.durability -= 10, item.enhancement -= 1;
+  } else if(enhancement > 14) {
+    item.durability -= 10; 
+  } else {
+    item.durability -= 5;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { 
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
+  const { enhancement } = item;
+  if(enhancement > 0) {
+    item.name = `[+${item.enhancement}] ${item.name}`
+  }
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,10 +1,107 @@
-const enhancer = require('./enhancer.js');
-const { TestScheduler } = require('jest');
-// test away!
-describe('enhanced items tests', () => {
+const { repair, succeed, fail, get } = require("./enhancer.js");
 
-test.todo('success');
-test.todo('failed');
-test.todo('repair');
-test.todo('get');
+describe("repair", () => {
+  it("should restore durability to 100", () => {
+    expect(
+      repair({
+        name: "Sword of Redux",
+        durability: 75,
+        enhancement: 5
+      })
+    ).toEqual({
+      name: "Sword of Redux",
+      durability: 100,
+      enhancement: 5
+    });
+  });
+});
+
+describe("succeed", () => {
+  it("should increment enhancement by 1 if less than 20", () => {
+    expect(
+      succeed({
+        name: "Hammer of Diligent Testing",
+        durability: 100,
+        enhancement: 19
+      })
+    ).toEqual({
+      name: "Hammer of Diligent Testing",
+      durability: 100,
+      enhancement: 20
+    });
+  });
+});
+
+describe("fail", () => {
+  it("should decrement durability by 10 and decrement enhancement by 1 if enhancement greater than 16", () => {
+    expect(
+      fail({
+        name: "Shield od Undefined",
+        durability: 90,
+        enhancement: 17
+      })
+    ).toEqual({
+      name: "Shield od Undefined",
+      durability: 80,
+      enhancement: 16
+    });
+  });
+
+  it("should decrement durability by 10 if enhancement greater than 14", () => {
+    expect(
+      fail({
+        name: "Wings of Passing Tests",
+        durability: 90,
+        enhancement: 15
+      })
+    ).toEqual({
+      name: "Wings of Passing Tests",
+      durability: 80,
+      enhancement: 15
+    });
+  });
+
+  it("should decrement durability by 5 if enhancement less than 15", () => {
+    expect(
+      fail({
+        name: "Greives of Failing",
+        durability: 90,
+        enhancement: 14
+      })
+    ).toEqual({
+      name: "Greives of Failing",
+      durability: 85,
+      enhancement: 14
+    });
+  });
+});
+
+describe('get', () => {
+    it('should modify name to prefix enhancement level if greater than 0', () => {
+        expect(
+            get({
+              name: "Lambdas Boots of Srpint",
+              durability: 90,
+              enhancement: 14
+            })
+          ).toEqual({
+            name: "[+14] Lambdas Boots of Srpint",
+            durability: 90,
+            enhancement: 14
+          });
+    })
+    it('should not modify name if enhancement level is 0', () => {
+        expect(
+            get({
+              name: "Pick of Axios",
+              durability: 90,
+              enhancement: 0
+            })
+          ).toEqual({
+            name: "Pick of Axios",
+            durability: 90,
+            enhancement: 0
+          });
+    })
+    
 })
