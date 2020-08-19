d76728f3ee53785353f8df3b7feda6e94a670380
Julian Leon <jleon1992@gmail.com>
2020-08-19 14:12:40 -0400

    tests written to make sure all four functions are working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,53 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement <= 20){
+    const newEnhancement = item.enhancement + 1
+    return { ...item, enhancement: newEnhancement}
+  } else {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    const newDurability = item.durability - 5
+    return {...item, durability: newDurability}
+  }else if(item.enhancement > 16){
+    const newEnhancement = item.enhancement - 1
+    const newDurability = item.durability - 10
+    return {
+      ...item, 
+      durability: newDurability,
+      enhancement: newEnhancement
+    }
+  } else {
+    const newDurability = item.durability - 10
+    return {...item, durability: newDurability}
+
+  }
+
 }
 
+
 function repair(item) {
-  return { ...item };
+  if(item.durability !== 100){
+
+    
+    return {...item, durability: 100}
+  } else {
+    return {...item}
+  }
+
 }
 
 function get(item) {
-  return { ...item };
+  if(item.enhancement > 0){
+    const enhanced = `[+${item.enhancement}] ${item.name}`
+
+    return { ...item, name: enhanced }
+  }else{
+    return { ...item };
+  }
+  
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,45 @@
 const enhancer = require('./enhancer.js');
+const { fail } = require('./enhancer.js');
 // test away!
+const item1 = {name: 'fidget spinner', durability: 90, enhancement: 0}
+const item2 = {name: 'Gameboy', durability: 50, enhancement: 15}
+
+describe('item module', () => {
+    describe('repair function', () => {
+        it('can repair the durability of item to 100', () => {
+            const expectedOutput = 100
+            const repairItem = enhancer.repair(item1)
+            const actualOutput = repairItem.durability
+
+            expect(actualOutput).toBe(expectedOutput)
+        })
+    })
+    describe('fail function', () => {
+        it('can modify an item according to the rules defined by client when it fails', () => {
+            const failItem = enhancer.fail(item2)
+
+            const actualDurabilityOutput = failItem.durability
+            const expectedDurability = failItem.durability = 40
+            expect(actualDurabilityOutput).toBe(expectedDurability)
+            expect(failItem).toBeDefined()
+            expect(failItem.durability).toBeLessThan(item2.durability)
+        })
+    })
+    describe('success function', () => {
+        it('will modify item correctly if it passes', () => {
+            const successItem = enhancer.success(item2)
+
+            const expectedEnhancement = successItem.enhancement = 16
+            const actualEnhancementOutput = successItem.enhancement
+            expect(expectedEnhancement).toBe(actualEnhancementOutput)
+            expect(successItem).toBeDefined()
+            expect(successItem.enhancement).toBeGreaterThan(item2.enhancement)
+        })
+    })
+    describe('get function', () => {
+        const enhancedName = enhancer.get(item2)
+        console.log(enhancedName.name)
+        expect(enhancedName).toBeDefined()
+    
+    })
+})
