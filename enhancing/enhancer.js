d05d43965ba516917acaa9818b1b3a172819e966
Darrell-Conklin <dmconklin84@gmail.com>
2020-09-10 19:26:41 -0500

    wrote some test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,25 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement >= 20) {
+    return item
+  } else {
+    return { ...item, enhancement: item.enhancement +1 };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 }
+  } else if (item.enhancement >= 15 ) {
+    return { ...item, durability: item.durability - 10 };
+  } else if (item.enhancement > 16) {
+    return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,30 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: "name",
+    durability: 20,
+    enhancement: 17
+}
+
+test("Repair Item Durability", () => {
+    expect(enhancer.repair(item)).toStrictEqual({ ...item, durability: 100 })
+})
+
+test("Successful Enhancement", () => {
+    if (item.enhancement >= 20) {
+        expect(enhancer.success(item)).toStrictEqual(item)
+    } else {
+        expect(enhancer.success(item)).toStrictEqual({ ...item, enhancement: 18})
+    }
+})
+
+test("Failed Enhancement", () => {
+    if (item.enhancement < 15) {
+        expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 15 })
+    } else if (item.enhancement >= 15 ) {
+        expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 10 })
+    } else if (item.enhancement > 16) {
+        expect(enhancer.fail(item)).toStrictEqual({ ...item, durability: 10, enhancement: 16 })       
+    }
+})
