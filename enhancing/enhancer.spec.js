b272bd4637faf2e63048939d122fc9fc31e79517
Evelyn Nguyen <Evelyn@viets-imacx.lan>
2020-09-10 17:33:00 -0700

    mpv

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
 const enhancer = require('./enhancer.js');
+const { timeStamp } = require('console');
+const { exportAllDeclaration } = require('@babel/types');
 // test away!
+const item = {
+    name: 'test',
+    durability: 10,
+    enhancement: 15
+}
+test("repair function", () => {
+    expect(enhancer.repair(item)).toStrictEqual({...item,durability:100})
+})
+
+test("success funtion", () => {
+    expect(enhancer.success(item)).toStrictEqual({
+        ...item,
+        enhancement:item.enhancement == 20 ? 20 : item.enhancement + 1
+    })
+})
+
+test("fail function", () => {
+    expect(enhancer.fail(item)).toStrictEqual({
+        ...item, 
+        durability: item.enhancement < 15 ? item.durability-5 : item.durability-10,
+        enhancement: item.enhancement > 16 ? item.enhancement-1 : item.enhancement  
+    })
+})
