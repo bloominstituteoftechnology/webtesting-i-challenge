6fe75190422bb6f9bc45edcb76b15aedbe765d2b
Stephen Gary <sgary0@protonmail.com>
2020-09-16 16:11:46 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,28 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement === 20) {
+    return { ...item };
+  } else {
+    item.enhancement++
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item, };
+  if(item.enhancement < 15) {
+    const durability = item.durability - 5;
+    return { ...item, durability: durability}
+  }
+  if(item.enhancement === 15) {
+    const durability = item.durability - 10;
+    return { ...item, durability: durability};
+  }
+  if(item.enhancement > 16) {
+    const enhancement = item.enhancement - 1;
+    const durability = item.durability - 10;
+    return { ...item, enhancement: enhancement, durability: durability};
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,8 +1,37 @@
 const { repair, fail, success, get } = require('./enhancer.js');
 
 describe('Repair', function() {
-  it('should restore durability to 100', function() {
-    const item = { name: 'pontiac', durability: 50,  enhancement: 20 }
+  it('items durability should be restored to 100', function() {
+    const item = { name: 'Dagger of sorrow', durability: 50,  enhancement: 20 }
     expect(repair(item)).toEqual(expect.objectContaining({ durability: 100 }))
+  })
+});
+
+describe('Success', function() {
+  it('items enhancement should increase by 1', function() {
+    const item = { name: 'Axe of destiny', durability: 100, enhancement: 15 }
+    expect(success(item)).toEqual(expect.objectContaining({ enhancement: 16 }))
+  })
+
+  it('items enhancement has a max of 20', function() {
+    const item = { name: 'Sword of truth', durability: 100, enhancement: 20 }
+    expect(success(item)).toEqual(expect.objectContaining({ enhancement: 20 }))
+  })
+});
+
+describe('Fail', function() {
+  it('if enhancement < 15, durability decreases by 5', function() {
+    const item = { name: 'Staff of revenge', durability: 100, enhancement: 12 }
+    expect(fail(item)).toEqual(expect.objectContaining({ durability: 95 }))
+  })
+
+  it('if enhancement > 15, durability decreases by 10', function() {
+    const item = { name: 'Sai of raphael', durability: 100, enhancement: 15 }
+    expect(fail(item)).toEqual(expect.objectContaining({ durability: 90 }))
+  })
+
+  it('if enhancement > 16, enhancement decreases by 1', function() {
+    const item = { name: 'Blade of the dragon', durability: 100, enhancement: 18 }
+    expect(fail(item)).toEqual(expect.objectContaining({ enhancement: 17 }))
   })
 });
