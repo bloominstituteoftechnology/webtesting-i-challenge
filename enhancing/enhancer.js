c600af52ac709cb676dc72fdb5a06a91ef17e8bb
unknown <nandhinimadan20@gmail.com>
2020-11-06 17:52:22 -0800

    completed repair

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,6 +4,10 @@ module.exports = {
   repair,
   get,
 };
+/*
+- Items have `name`, `durability` and `enhancement`.
+- The item's `enhancement` it's a number from 0 to 20.
+- The item's `durability` it's a number from 0 to 100.*/
 
 function success(item) {
   return { ...item };
@@ -12,9 +16,13 @@ function success(item) {
 function fail(item) {
   return { ...item };
 }
-
+// a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. 
+//This method is the simplest of the three and would be a **good starting point** on this project.
 function repair(item) {
-  return { ...item };
+  item.durability=100;
+console.log(item.durability)
+
+  return { ...item};
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,8 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("testing",()=>{
+    it ("repair item",()=>{
+        item={name:"nandhini",durability:60,enhancement:60}
+        expect(enhancer.repair(item)).toEqual({ name:"nandhini",durability:100,enhancement:60 })
+    })
+})
