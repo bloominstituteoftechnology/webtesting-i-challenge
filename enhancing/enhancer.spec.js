ddcbd6fdf4e725165d5310bdc56ce1f9ec92dfb5
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-11-11 17:17:01 -0500

    mvp met

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const dummyItem = {
+  name: 'Skelator',
+  durability: 66,
+  enhancement: 11,
+}
+const dummyItem2 = { name: "Battle Cat", durability: 100, enhancement: 6 }
+
+it('test for sanity', function () {
+  expect(true).toBe(true);
+});
+
+describe('repair', () => {
+  it('the durability should be 100', () => {
+    expect(() => {
+      enhancer.repair(dummyItem).toBe(dummyItem.durability == 100);
+    });
+  });
+});
+
+describe('success', () => {
+  it('enhancement is succesful', () => {
+
+    expect(() => {
+      enhancer.success(dummyItem).toEqual(dummyItem2)
+    })
+  })
+})
+
+describe('failure', () => {
+  it('enhancement has failed', () => {
+    expect(() => {
+      enhancer.failure(dummyItem).toEqual(dummyItem)
+    })
+  })
+})
