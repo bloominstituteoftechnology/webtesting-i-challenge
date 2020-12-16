81505c0ee2fd5aaf61685ce516a01494a9d35159
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-12-16 13:59:21 -0600

    finished mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,9 +1,42 @@
-module.exports = {
-  success,
-  fail,
-  repair,
-  get,
-};
+
+class theItem {
+  constructor(itemName, durability, enchantment) {
+    this.itemName = itemName,
+      this.durability = durability,
+      this.enchantment = enchantment
+  }
+  repair(item) {
+    return { ...item, durability: 100 };
+  }
+  success(item) {
+    const Ecount = item.enchantment
+    if (item.enchantment >= 20) {
+      return { ...item, enchantment: enchantment };
+    } else {
+      let Ecount = item.enchantment += 1
+      return { ...item, enchantment: Ecount }
+    }
+  }
+  fail(item) {
+    if (item.enchantment < 15) {
+      const enchant = item.durability -= 5
+      return { ...item, durability: enchant }
+    } else if (item.enchantment = 15) {
+      const dechant = item.durability -= 10
+      return { ...item, durability: dechant }
+    } else if (item.enchantment >= 16) {
+      const fail = item.durability -= 10
+      const failE = item.enchantment -= 1
+      return { ...item, durability: fail, enchantment: failE }
+    } else {
+      console.log('wtf')
+    }
+  }
+}
+
+
+
+
 
 function success(item) {
   return { ...item };
@@ -14,9 +47,12 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, [durability]: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+module.exports = {
+  theItem
+};
