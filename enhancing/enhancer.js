fadcf1b22dd8eef7a8236d67438f83852b7ecfc8
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-12-15 23:21:04 -0500

    MVP done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,30 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
-}
+  if(item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return { ...item }
+  }
+};
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement === 15 ) {
+    return { ...item, durability: item.durability - 10 };
+  } else if (item.enhancement > 16) {
+    return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 };
+  }
+};
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
+  
 }
 
 function get(item) {
   return { ...item };
 }
+
+
