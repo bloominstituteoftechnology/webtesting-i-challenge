5df08bf2cb8dacdc20baf38356fab48caeffc6dd
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-12-17 10:03:19 -0500

    PR cleanup

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,27 +9,24 @@ function success(item) {
   if(item.enhancement < 20) {
     return { ...item, enhancement: item.enhancement + 1 };
   } else {
-    return { ...item }
-  }
+    return { ...item };
+  };
 };
 
 function fail(item) {
   if (item.enhancement < 15) {
     return { ...item, durability: item.durability - 5 };
-  } else if (item.enhancement === 15 ) {
+  } else if (item.enhancement === 15) {
     return { ...item, durability: item.durability - 10 };
   } else if (item.enhancement > 16) {
     return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 };
-  }
+  };
 };
 
 function repair(item) {
   return { ...item, durability: 100 };
-  
-}
+};
 
 function get(item) {
   return { ...item };
-}
-
-
+};
