dcb1673f723d978be58a6b062663cd90063bbddb
Elizabeth Hagag <lizjanegiles@gmail.com>
2021-01-19 19:25:52 -0600

    test working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,31 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return {...item, enhancement: item.enhancement + 1}
+    
+  } else {
+    return {...item, enhancement: item.enhancement}
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+    return {...item, durability:item.durability - 5}
+  }
+  if(item.enhancement >= 15 || item.enhancement >= 16 ) {
+    return {...item, durability: item.durability - 10}
+  }
+  if(item.enhancement >= 16) {
+    return {...item, enhancement: item.enhancement - 1}
+  } else {
+    return {...item, durability: item.durability, enhancement: item.enhancement}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+item.durability = 100
+return {...item}
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,30 @@
-const enhancer = require('./enhancer.js');
+const {success,fail,repair} = require('./enhancer.js');
 // test away!
+
+let item = {name: "Elizabeth", durability: 5, enhancement: 10}
+
+describe("success", () => {
+    it("should return a successful test with enhancement increased by 1 if less than 20",() =>{
+        let testSuccess = {name: "Elizabeth", durability: 5, enhancement: 11};
+        expect(success(item)).toEqual(testSuccess)
+    })
+})
+
+describe("fails", () => {
+    it("should decress durability 5 less than", () => {
+        let testFailOne = {name: "Elizabeth", durability: 0, enhancement: 10}
+        expect(fail(item)).toEqual(testFailOne)
+    })
+    it("should decress durabilty by 10 if equal or greater than 15", () => {
+        let itemTwo = {name: "Elizabeth", durability: 10, enhancement: 15}
+        let testFailTwo = {name: "Elizabeth", durability: 0, enhancement: 15}
+        expect(fail(itemTwo)).toEqual(testFailTwo)
+    })
+})
+
+describe("repairs", () => {
+    it("changes durability to 100" , () => {
+        let testRepair = {name: "Elizabeth", durability: 100, enhancement: 10}
+        expect(repair(item)).toEqual(testRepair)
+    })
+})
