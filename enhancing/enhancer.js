47838819038cbeada9682d8d10fcc6a815733653
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-01-27 22:21:25 -0500

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -17,19 +17,21 @@ function success(item) {
 
 function fail(item) {
   if (item.enhancement < 15) {
-    item.durability - 5
 
-    if (item.enhancement >= 15) {
-      item.durability - 10
-    }
+     item.durability -= 5
 
-    if (item.enhancement > 16) {
-      item.enhancement - 1
-    }
+  } else if (item.enhancement >= 15) {
+
+      item.durability -= 10
+
+  } else if (item.enhancement > 16) {
+
+      item.enhancement -= 1
+
   } else {
+
     return { ...item };
   }
-  
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -36,17 +36,29 @@ describe('enhancer success', () => {
 })
 
 describe('enhancer fail', () => {
-    let item
+    let item, item2, item3
     beforeEach(() => {
-      item = { name: "sword", enhancement: 0, durability: 100 }
+      item = { name: "sword", enhancement: 0, durability: 100} 
+      item2 = { name: "gun", enhancement: 15, durability: 100 }
+      item3 = { name: "dragon", enhancement: 20, durability: 100 }
     })
 
     it('fails exists', () => {
         expect(enhancer.fail).toBeDefined()
     })
     it('subtracts 5 from durability when enhancement less than 15', () => {
-        expect(item.enhancement).toBe(0)
+        expect(item.durability).toBe(100)
         enhancer.fail(item)
-        expect(item.durability).toBe(85)
+        expect(item.durability).toBe(95)
+    })
+    it('subtracts 10 durability if enhancement is greater than or equal to 15', () => {
+        expect(item2.durability).toBe(100)
+        enhancer.fail(item2)
+        expect(item2.durability).toBe(90)
+    })
+    it('subtracts 1 enhancement when enhancement is greater than 16', () =>{
+        expect(item3.enhancement).toBe(20)
+        enhancer.fail(item3)
+        expect(item3.enhancement).toBe(19)
     })
 })
