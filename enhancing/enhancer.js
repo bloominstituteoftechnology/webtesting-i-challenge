85c9b22de23e6813cc2f925abd43d14cdad9c4e2
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-02-16 19:22:15 -0800

    repair fmethod works

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,6 +5,7 @@ module.exports = {
   get,
 };
 
+
 function success(item) {
   return { ...item };
 }
@@ -14,8 +15,18 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+
+  return  {...item, durability:100}
+
 }
+
+//- a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
+
+// ### Items
+
+// - Items have `name`, `durability` and `enhancement`.
+// - The item's `enhancement` it's a number from 0 to 20.
+// - The item's `durability` it's a number from 0 to 100.
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,48 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+//success function
+
+describe('success function', () => {
+    test('success function exists', () => {
+        expect(enhancer.success).toBeDefined();
+    })
+});
+
+//fail test
+
+describe('fail function', () => {
+    test('fail function exists', () => {
+        expect(enhancer.fail).toBeDefined();
+    })
+});
+
+//repair test
+
+describe('repair function', () => {
+    it('Test that the repair method works', () => {
+        const newItem = {
+            name: "Metroid Prime",
+            durability: 1,
+            enhancement: 15,
+        };
+        const expectedItem = {
+            name: "Metroid Prime",
+            durability: 100,
+            enhancement: 15,
+        };
+
+        const item = enhancer.repair(newItem)
+        expect(item.name).toBe(expectedItem.name);
+        expect(item.durability).toBe(expectedItem.durability);
+        expect(item.enhancement).toBe(expectedItem.enhancement);
+    })
+});
+
+describe('get function', () => {
+    test('get function exists', () => {
+        expect(enhancer.get).toBeDefined();
+    })
+});
+
+/// AAA = Arrange -> Act -> Assess
