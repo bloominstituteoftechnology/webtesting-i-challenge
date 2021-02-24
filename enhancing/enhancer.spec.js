1ed510d89c3207fff594d7477638e8b2864e4908
Diego Roman <diegormnv209@gmail.com>
2021-02-24 12:21:15 -0800

    installed dependencies and wrote some basic tests to see if it is working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,32 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+it('works', () =>{
+    expect(1).not.toBe(2)
+})
+
+describe('item class', () =>{
+    let item ={}
+    beforeEach(() =>{
+        item = {
+            name: 'Sword',
+            enchantment: 5,
+            durability: 50
+        }
+    })
+
+    it('exists', () =>{
+        expect(enhancer).toBeDefined()
+    })
+
+    it('item has a name', () =>{
+        expect(item).toHaveProperty('name', 'Sword')
+    })
+
+    it('item has an enchantment', () =>{
+        expect(item).toHaveProperty('enchantment', 5)
+    })
+
+    it('item has durability', () =>{
+        expect(item).toHaveProperty('durability', 50)
+    })
+})
